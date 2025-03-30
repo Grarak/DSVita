65e810d2d7bcae575aa90e8c783261986a3bbfa7
Willi Ye <williye97@gmail.com>
2025-03-30 19:52:01 +0200

    hle: Always reply to mic request

--- src/core/hle/mic_hle.rs
+++ src/core/hle/mic_hle.rs
@@ -18,14 +18,17 @@ impl MicHle {
 
         self.data[((data >> 16) & 0xF) as usize] = data as u16;
 
-        if !(data & (1 << 24)) != 0 {
+        if (data & (1 << 24)) == 0 {
             return;
         }
 
         let cmd = (self.data[0] >> 8) - 0x40;
         match cmd {
             0 => Arm7Hle::send_ipc_fifo(IpcFifoTag::Mic, 0x0300C000, false, emu),
-            _ => debug_println!("unknown mic request {data:x}"),
+            _ => {
+                Arm7Hle::send_ipc_fifo(IpcFifoTag::Mic, 0x0300C000, false, emu); // Just send same reply, seems to work
+                debug_println!("unknown mic request {data:x}");
+            }
         }
     }
 }
